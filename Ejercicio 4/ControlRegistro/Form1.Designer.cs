c634c7bccf2de59842fdade3a24c2c3a4543e945
Erick-Telle <ebguido@uamv.edu.ni>
2024-09-20 10:46:18 -0600

    Add files via upload

---
+++ Ejercicio 4/ControlRegistro/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ControlRegistro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(79, 12);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(115, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(79, 46);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(116, 20);
+            this.txtNick.TabIndex = 4;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(79, 74);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(115, 20);
+            this.txtContraseña.TabIndex = 5;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(236, 25);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(132, 64);
+            this.btnRegistro.TabIndex = 6;
+            this.btnRegistro.Text = "Registrar";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnRegistro;
+    }
+}
+
---
+++ Ejercicio 4/ControlRegistro/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlRegistro
+{
+    public partial class Form1 : Form
+    {
+        private List<User> usuariosRegistrados = new List<User>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Funcion de boton regristo
+        private void btnRegistro_Click(object sender, EventArgs e)
+        {
+
+            string nombre = txtNombre.Text;
+            string nick = txtNick.Text;
+            string contraseña = txtContraseña.Text;
+            string contraseñaCorrecta = "1234";
+            string id = "12";
+            int i = 0;
+
+            //Se encarga de verificar que no falten datos requeridos 
+            if (!string.IsNullOrEmpty(nombre) && !string.IsNullOrEmpty(nick) && !string.IsNullOrEmpty(contraseña))
+            {
+                //Verifica que el programa no exeda el limite de registro
+                if (usuariosRegistrados.Count < 100)
+                {
+                    // Verificar si el usuario ya existe en la lista
+                    bool usuarioExiste = usuariosRegistrados.Any(u => u.Nick == nick);
+
+                    i++;
+                    if (i > 2)
+                    {
+                        MessageBox.Show("Demasiados intentas, intentelo mas tarde");
+                        btnRegistro.Enabled = false;
+                    }
+                    if (contraseña != contraseñaCorrecta)
+                    {
+                        MessageBox.Show("Contraseña incorrecta");
+                    }
+                    else
+                    {
+                        if (!usuarioExiste)
+                        {
+                            User usuario = new User { Nombre = nombre, Nick = nick, Contraseña = contraseña };
+                            usuariosRegistrados.Add(usuario);
+                            MessageBox.Show("Usuario registrado con éxito!");
+                            txtNombre.Clear();
+                            txtNick.Clear();
+                            txtContraseña.Clear();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+            }
+        }
+    }
+    //Clase, por que se me hiso mas facil haci.
+    public class User
+    {
+        public string Nombre { get; set; }
+        public string Nick { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
